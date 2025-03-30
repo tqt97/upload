c4979791301dd51fcc791cabd51fc0bfce0074a8
TuanTQ <32692426+tqt97@users.noreply.github.com>
2025-03-30 17:01:27 +0700

    Create controller.php

---
+++ v2/controller.php
@@ -0,0 +1,35 @@
+<?php
+class Controller_Import extends Controller
+{
+    public function action_import()
+    {
+        $success_count = 0;
+
+        if (\Input::method() == 'POST' && \Input::file('file_input')) {
+            // Dùng generator cho dữ liệu lớn, hoặc mảng cho dữ liệu nhỏ
+            $use_generator = true; // Bật nếu file lớn
+            $data = \Helper\Import::importFile(\Input::file('file_input'), $use_generator);
+
+            $dataController = new Controller_Data();
+            if ($use_generator) {
+                foreach ($data as $row) {
+                    if ($dataController->action_store($row)) {
+                        $success_count++;
+                    }
+                }
+            } else {
+                foreach ($data as $row) {
+                    if ($dataController->action_store($row)) {
+                        $success_count++;
+                    }
+                }
+            }
+
+            if ($success_count > 0) {
+                \Session::set_flash('success', "Đã import thành công $success_count bản ghi!");
+            }
+        }
+
+        return \Response::forge(\View::forge('import/import'));
+    }
+}
